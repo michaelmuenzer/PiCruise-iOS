582a4a831eaad3a04cdae5533839c852494cd28e
Michael Muenzer <michael.muenzer@gmail.com>
2021-01-03 13:27:15 +0100

    Initial Commit

---
+++ picruise/picruiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  picruiseApp.swift
+//  picruise
+//
+//  Created by Michael Münzer on 03.01.21.
+//
+
+import SwiftUI
+
+@main
+struct picruiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
